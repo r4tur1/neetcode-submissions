8efeea0d14f4c033aae56229da7862ba500d331d
r4tur1 <aadityaraturi11@gmail.com>
2026-07-11 22:07:09 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 1.23
+print(type(variable))
+variable = True
+print(type(variable))
+variable = 'hello'
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
