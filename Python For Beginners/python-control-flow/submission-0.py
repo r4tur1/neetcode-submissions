7a27b563ed1c38b74aec2ed934c35fc5c3a2d563
r4tur1 <aadityaraturi11@gmail.com>
2026-07-13 12:26:45 +0530

    Add: python-control-flow - submission-0

---
+++ Python For Beginners/python-control-flow/submission-0.py
@@ -0,0 +1,14 @@
+for i in range(1, 8):
+    pass
+
+if True:
+    pass
+
+def unfinsished_function():
+    pass
+
+for i in range(1, 8):
+    pass
+for i in range(1, 8):
+    pass
+print("nothing else happened")
