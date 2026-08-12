3a2b8066282e1cc329db9c243a9905d318f99396
r4tur1 <aadityaraturi11@gmail.com>
2026-08-12 22:47:31 +0530

    Add: python-list-clone - submission-1

---
+++ Python Coding Interviews/python-list-clone/submission-1.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+def remove_element(arr: List[int], element: int) -> List[int]:
+    cloned_list = arr.copy()
+    for i in cloned_list:
+        if i == element:
+            cloned_list.remove(i)
+            break
+    return cloned_list
+
+
+
+# do not modify below this line
+arr = [1, 3, 5, 7, 9]
+
+print(remove_element(arr, 3))
+print(arr)
+print(remove_element(arr, 9))
+print(arr)
+print(remove_element(arr, 1))
+print(arr)
